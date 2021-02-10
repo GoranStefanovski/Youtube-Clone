bb14b7262392dd81f423bea61449e9d16cc5956d
Goran Stefanovski <tevidma@gmail.com>
2021-02-10 12:59:50 +0100

    First Commit

---
+++ src/SideBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./SideBar.css";
+import WhatshotIcon from '@material-ui/icons/Whatshot';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+
+function SideBar() {
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default SideBar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBxY2OAcmkZOrmz7KQRvbUaCf4p-07ZtP0",
+    authDomain: "clone-44cf7.firebaseapp.com",
+    projectId: "clone-44cf7",
+    storageBucket: "clone-44cf7.appspot.com",
+    messagingSenderId: "326718533443",
+    appId: "1:326718533443:web:bb1e41a989913d4b7c09f6",
+    measurementId: "G-DCQZ1NS0EM"
+}; 
