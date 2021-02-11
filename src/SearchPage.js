a4537f282881a28cccfd442144665334fda619ee
Goran Stefanovski <tevidma@gmail.com>
2021-02-11 12:27:27 +0100

    Finished

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./SearchPage.css";
 import ChannelRow from "./ChannelRow";
+import VideoRow from "./VideoRow"
 import TuneOutlinedIcon from '@material-ui/icons/TuneOutlined';
 
 
@@ -19,6 +20,54 @@ function SearchPage() {
                 subs="6.98M"
                 noOfVideos={1699}
                 description="The Official Metallica Music Channel"
+            />
+            <hr />
+
+            <VideoRow
+                views="1.4M"
+                subs="659"
+                description="Playing Minecraft"
+                timestamp="59 seconds ago"
+                channel="Pewdiepie"
+                title="Let's play Minecraft"
+                image="https://i.redd.it/9p0avomgt5631.jpg"
+            />
+
+            <VideoRow
+                views="1.4M"
+                subs="659"
+                description="Playing Minecraft"
+                timestamp="59 seconds ago"
+                channel="Pewdiepie"
+                title="Let's play Minecraft"
+                image="https://i.redd.it/9p0avomgt5631.jpg"
+            />
+            <VideoRow
+                views="1.4M"
+                subs="659"
+                description="Playing Minecraft"
+                timestamp="59 seconds ago"
+                channel="Pewdiepie"
+                title="Let's play Minecraft"
+                image="https://i.redd.it/9p0avomgt5631.jpg"
+            />
+            <VideoRow
+                views="1.4M"
+                subs="659"
+                description="Playing Minecraft"
+                timestamp="59 seconds ago"
+                channel="Pewdiepie"
+                title="Let's play Minecraft"
+                image="https://i.redd.it/9p0avomgt5631.jpg"
+            />
+            <VideoRow
+                views="1.4M"
+                subs="659"
+                description="Playing Minecraft"
+                timestamp="59 seconds ago"
+                channel="Pewdiepie"
+                title="Let's play Minecraft"
+                image="https://i.redd.it/9p0avomgt5631.jpg"
             />
         </div>
     )
